d3ca86ba84fc258fb58a4f76502098d5f2029443
hansi <hansi@bihalu.de>
2024-04-03 11:03:05 +0200

    ~ comment

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -10,6 +10,7 @@ wget https://github.com/aquasecurity/trivy/releases/download/v$VERSION/trivy_${V
 # Get all cluster images
 IMAGES=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec['initContainers', 'containers'][*].image}" | tr -s '[[:space:]]' '\n' | sort -u)
 
+# Scan images for critical vulnerabilities
 for IMAGE in $IMAGES ; do
   gum spin --title "Scanning image $IMAGE" -- trivy image --quiet --severity CRITICAL --output /tmp/$RESULT $IMAGE 
   cat /tmp/$RESULT >> $RESULT
