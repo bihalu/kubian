a7e159992c90707e8de5f61762e7e52f2f36c438
hansi <hansi@bihalu.de>
2024-04-03 11:12:29 +0200

    ~ download trivy

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -5,7 +5,7 @@ VERSION="0.50.1"
 RESULT="scan-"`date +"%Y-%m-%d-%H%M"`".txt"
 
 # Download trivy scanner
-wget https://github.com/aquasecurity/trivy/releases/download/v$VERSION/trivy_${VERSION}_Linux-64bit.tar.gz -O trivy && mv trivy /usr/local/bin/
+wget https://github.com/aquasecurity/trivy/releases/download/v$VERSION/trivy_${VERSION}_Linux-64bit.tar.gz -O - | tar -xz trivy && mv trivy /usr/local/bin/
 
 # Get all cluster images
 IMAGES=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec['initContainers', 'containers'][*].image}" | tr -s '[[:space:]]' '\n' | sort -u)
